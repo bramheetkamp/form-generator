66790979548ecedaecde64e5443d630a5af9e159
ItsNotDaan <513640@student.saxion.nl>
2025-11-13 11:49:44 +0100

    Add localization files and update overview page with new intake buttons

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const baseConfigs = {
     NEXT_PUBLIC_CROSS_SITE_COOKIES: process.env.NEXT_PUBLIC_CROSS_SITE_COOKIES,
     NEXT_PUBLIC_PROTO: process.env.NEXT_PUBLIC_PROTO,
   },
+  webpack: (config) => {
+    config.resolve.alias['@next-translate-root/locales'] = require('path').resolve(__dirname, 'locales');
+    return config;
+  },
 };
 
 module.exports = baseConfigs;
--- src/presentation/overview/overview.tsx
+++ src/presentation/overview/overview.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {BaseLayout} from '@/presentation/base/baseLayout';
-import {Button, Divider, Flex, SimpleGrid, Text} from '@chakra-ui/react';
+import { BaseLayout } from '@/presentation/base/baseLayout';
+import { Button, Divider, Flex, SimpleGrid, Text } from '@chakra-ui/react';
 import useTranslation from 'next-translate/useTranslation';
-import {useRouter} from 'next/router';
-import {Routes} from '../routes';
+import { useRouter } from 'next/router';
+import { Routes } from '../routes';
 
 export const OverviewPage = () => {
   const router = useRouter();
@@ -24,7 +24,7 @@ export const OverviewPage = () => {
           {'forms'}
         </Text>
 
-        <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={4}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
           <Button
             variant={'primary'}
             p={4}
@@ -51,34 +51,71 @@ export const OverviewPage = () => {
       <Divider />
 
       <Flex
-        gap={{base: 4, md: 6}}
-        direction="column"
-        border="1px solid"
-        borderColor="inherit"
-        borderRadius="md"
+        w={'full'}
+        h={'full'}
+        bg={'white'}
+        borderRadius={'2'}
+        flex={'1'}
         p={4}
-        mt={2}
+        direction="column"
+        gap={6}
       >
-        <Button
-          variant={'secondary'}
-          p={4}
-          onClick={event => {
-            event.preventDefault();
-            router.push(Routes.form_intake_vlos);
-          }}
-        >
-          Intake VLOS
-        </Button>
-        <Button
-          variant={'secondary'}
-          p={4}
-          onClick={event => {
-            event.preventDefault();
-            router.push(Routes.form_intake_osa_vlos);
-          }}
-        >
-          Intake OSA/VLOS
-        </Button>
+        <Text variant={'title'} w={'full'} noOfLines={1}>
+          {'Intake Formulieren'}
+        </Text>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
+          <Button
+            variant={'secondary'}
+            p={4}
+            onClick={event => {
+              event.preventDefault();
+              router.push(Routes.form_intake_vlos);
+            }}
+          >
+            Intake VLOS
+          </Button>
+          <Button
+            variant={'secondary'}
+            p={4}
+            onClick={event => {
+              event.preventDefault();
+              router.push(Routes.form_intake_pulman);
+            }}
+          >
+            Intake Pulman
+          </Button>
+          <Button
+            variant={'secondary'}
+            p={4}
+            onClick={event => {
+              event.preventDefault();
+              router.push(Routes.form_intake_osb);
+            }}
+          >
+            Intake OSB
+          </Button>
+          <Button
+            variant={'secondary'}
+            p={4}
+            onClick={event => {
+              event.preventDefault();
+              router.push(Routes.form_intake_ovac);
+            }}
+          >
+            Intake OVAC
+          </Button>
+          <Button
+            variant={'secondary'}
+            p={4}
+            onClick={event => {
+              event.preventDefault();
+              router.push(Routes.form_intake_steunzolen);
+            }}
+          >
+            Intake Steunzolen
+          </Button>
+        </SimpleGrid>
       </Flex>
     </BaseLayout>
   );
